19339df957fa650c132446bc96c29fa67a27b3f5
dviramontes <dviramontes@gmail.com>
2020-12-10 20:49:58 -0700

    adds GET /ping

--- src/clj/api/core.clj
+++ src/clj/api/core.clj
@@ -1,4 +1,23 @@
-(ns api.core)
+(ns api.core
+  (:require [org.httpkit.server :refer [run-server]]
+            [reitit.ring :as ring]))
+
+(defonce server (atom nil))
+
+(defn stop-server []
+  (when-not (nil? @server)
+    (@server :timeout 100)
+    (reset! server nil)))
+
+(def app
+  (ring/ring-handler
+    (ring/router
+      [;; obligatory health check
+       ["/ping" {:get (fn [_] {:status 200 :body "pong"})}]])))
 
 (defn -main []
-  (println "hello world!"))
+  (println "server started")
+  (reset! server (run-server app {:port 4000})))
+
+(comment
+  (-main))
