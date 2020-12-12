6c613cd6cb76889e8b4341be945eeb345109e7e0
dviramontes <dviramontes@gmail.com>
2020-12-12 01:42:40 -0700

    setup json middleware

--- src/clj/api/core.clj
+++ src/clj/api/core.clj
@@ -1,8 +1,16 @@
 (ns api.core
-  (:require [org.httpkit.server :refer [run-server]]
-            [reitit.ring :as ring]
-            [aero.core :as aero]
-            [clojure.java.io :as io]))
+    (:require
+      [org.httpkit.server :refer [run-server]]
+      [reitit.ring :as ring]
+      [aero.core :as aero]
+      [clojure.java.io :as io]
+      [muuntaja.core :as m]
+      [reitit.ring.middleware.exception :as exception-handler]
+      [reitit.ring.middleware.muuntaja :refer [format-negotiate-middleware
+                                               format-request-middleware
+                                               format-response-middleware]]))
+
+(declare -main)
 
 (defonce server (atom nil))
 
@@ -14,11 +22,25 @@
     (@server :timeout 100)
     (reset! server nil)))
 
+(defn restart-server []
+  (stop-server)
+  (-main))
+
 (def app
   (ring/ring-handler
     (ring/router
       [;; obligatory health check
-       ["/ping" {:get (fn [_] {:status 200 :body "pong"})}]])))
+       ["/ping" {:get (fn [_] {:status 200 :body "pong"})}]
+       ["/api" {:get (fn [_] {:status 200 :body {:hello "world"}})}]]
+      {:data {:muuntaja   m/instance
+              :middleware [format-negotiate-middleware
+                           format-response-middleware
+                           exception-handler/exception-middleware
+                           format-request-middleware]}})
+    (ring/routes
+      (ring/redirect-trailing-slash-handler)
+      (ring/create-default-handler
+        {:not-found (constantly {:status 404 :body "route not found"})}))))
 
 (defn -main []
   (let [c (config)]
