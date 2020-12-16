87612d2bdfb5f6d5c56741336d80cc1a16f526c4
dviramontes <dviramontes@gmail.com>
2020-12-16 01:30:08 -0700

    adds recursive call to get all product data from bc API

--- src/clj/api/bigcommerce.clj
+++ src/clj/api/bigcommerce.clj
@@ -1,5 +1,6 @@
 (ns api.bigcommerce
-    (:require [org.httpkit.client :as http]))
+    (:require [org.httpkit.client :as http]
+              [clojure.data.json :as json]))
 
 (def includes "include=images,variants")
 
@@ -8,17 +9,30 @@
   {:products "https://api.bigcommerce.com/stores/%s/v3/catalog/products?limit=%d&page=%d&%s"
    :product  "https://api.bigcommerce.com/stores/%s/v3/catalog/products/%d?%s"})
 
-;; TODO: recursively get all products
+(defn with-auth-headers [client token]
+  {"X-Auth-Client" client
+   "X-Auth-Token"  token})
+
 (defn fetch-products
-  "fetch all products from bigcommerce API"
-  [{:keys [store limit page]}]
-  (let [endpoint (format (endpoints :products) store limit page includes)]
-       endpoint))
+  "recursive call to fetch all products from bigcommerce API"
+  [{:keys [client token store] :as config} page limit & [products]]
+  (let [opts {:headers (with-auth-headers client token)}
+        endpoint (format (endpoints :products) store limit page includes)
+        {:keys [body error status]} @(http/get endpoint opts)]
+       (if (and (not error) (= status 200))
+         (let [payload (json/read-str body :key-fn keyword)
+               {:keys [current_page total_pages]} (-> payload :meta :pagination)
+               next-products (-> products
+                                 (update :data concat (:data payload))
+                                 (update :meta merge (:meta payload)))]
+              (if (not= current_page total_pages)
+                (fetch-products config (+ 1 page) limit next-products)
+                next-products))
+         error)))
 
 (defn fetch-product
   "fetch product info from bigcommerce API by product id"
   [{:keys [client token store]} id]
-  (let [opts {:headers {"X-Auth-Client" client
-                        "X-Auth-Token"  token}}
+  (let [opts {:headers (with-auth-headers client token)}
         endpoint (format (endpoints :product) store id includes)]
     @(http/get endpoint opts)))
--- src/clj/api/handlers.clj
+++ src/clj/api/handlers.clj
@@ -14,6 +14,13 @@
   (let [products (db/get-products db/config)]
     {:status 200 :body products}))
 
+(defn get-products-test [req]
+  (let [config (get-in req [:reitit.core/match :data :config :bigcommerce])]
+       ;; TODO: update db with prod info from API instead of retuning product info here
+       (if-let [products (bc/fetch-products config 1 400)]
+         {:status 200 :body products}
+         {:status 500 :body {:error "error failed to get paginated set of products from bigcommerce API"}})))
+
 (defn update-product [req]
   ;; TODO: get product id from ProductWebhookPayload
   (let [config (get-in req [:reitit.core/match :data :config :bigcommerce])
